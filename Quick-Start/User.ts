f180e56751fe0370293fd12e398369bd5b8d3e6e
jonas-sebera <sebejaz99@gmail.com>
2023-12-16 09:34:48 +0200

    update typeSystem

--- Quick-Start/User.ts
+++ Quick-Start/User.ts
@@ -87,3 +87,23 @@ interface BackPack<Type> {
 declare const backpack: BackPack<string>;
 const object = backpack.get();
 backpack.add("name");
+
+/**
+ * STRUCTURAL TYPE SYSTEM
+ *Type checking focuses on the shape that the values have
+ *sometimes called "duck typing" | "structural typing"
+ *In structural system, if two objects have the same shape, they are considered to be of the same type
+ */
+
+ interface Point {
+    x: number;
+    y: number;
+ }
+
+ function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+ }
+
+//  logs "12, 26"
+const point = { x: 12, y: 26};
+logPoint(point);
