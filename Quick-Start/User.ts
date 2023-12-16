51da2e5113de60feb9f7bbfa801c164ee3fc5bdf
jonas-sebera <sebejaz99@gmail.com>
2023-12-16 08:30:12 +0200

    init interface

---
+++ Quick-Start/User.ts
@@ -0,0 +1,15 @@
+const user = {
+    name: "Hayes",
+    id: 0,
+};
+
+// interface  case
+interface User {
+    name: string,
+    id: number,
+};
+
+/*Since JavaScript supports classes and object-oriented programming, 
+*so does TypeScript. You can use an interface declaration with classes: */
+
+ 
