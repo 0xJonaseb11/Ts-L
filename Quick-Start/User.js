516eca414f749d005423ac0270613b31aea7e5a4
jonas-sebera <sebejaz99@gmail.com>
2023-12-16 10:16:17 +0200

    user.js update

--- Quick-Start/User.js
+++ Quick-Start/User.js
@@ -32,3 +32,19 @@ function logPoint(p) {
 //  logs "12, 26"
 var point = { x: 12, y: 26 };
 logPoint(point);
+/*
+ * The point variable is never declared
+ *  to be a Point type. However, TypeScript
+ *  compares the shape of point to the shape
+ *  of Point in the type-check. They have the same
+ *  shape, so the code passes.
+ *
+ * The shape-matching only requires a subset of the object’s fields to match.
+ *
+ */
+var point3 = { x: 12, y: 26, z: 89 };
+logPoint(point3); // logs "12, 26"
+var rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect); // logs "33, 3"
+var color = { hex: "#187ABF" };
+logPoint(point3);
