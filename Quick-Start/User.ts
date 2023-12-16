0d888b270520b16743af7c14c8bf4b26e44b324f
jonas-sebera <sebejaz99@gmail.com>
2023-12-16 08:38:29 +0200

    init functions

--- Quick-Start/User.ts
+++ Quick-Start/User.ts
@@ -12,12 +12,12 @@ interface User {
 /*Since JavaScript supports classes and object-oriented programming, 
 *so does TypeScript. You can use an interface declaration with classes: */
 
- interface User {
+interface User {
     name: string,
     id: number
  };
 
- class UserAccount {
+class UserAccount {
     name: string;
     id: number;
 
@@ -25,6 +25,15 @@ interface User {
         this.name = name;
         this.id = id;
     }
- }
+}
 
- const user: User = new UserAccount("Murphy", 1);
+const user: User = new UserAccount("Murphy", 1);
+
+ // delete the user
+ function deleteUser(User: User) {
+    // ...
+ }
+  // get adminUser
+ function getAdminUser() {
+    // ...
+ }
